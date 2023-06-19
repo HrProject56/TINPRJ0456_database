e87a00d68eadcf7e1e2bd6574e9fd999e94c0b7a
RFlintstone <rubenflint@hotmail.com>
2023-06-19 16:08:22 +0200

    Fixed prod/dev modes with pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,15 @@ module.exports = {
             max_memory_restart: '1G',
             exec_mode: 'fork',
             interpreter: 'node',
-            interpreter_args: '-r ts-node/register'
+            interpreter_args: '-r ts-node/register',
+            env: {
+                "EXPRESS_PORT": 3000,
+                "NODE_ENV": "development"
+            },
+            env_production: {
+                "EXPRESS_PORT": 8000,
+                "NODE_ENV": "production",
+            }
         }
     ]
 };
